fbf2a1d640dc4c4731cf182458e935106747776a
Rommel Santor <rommel@rommelsantor.com>
2015-10-22 12:31:32 -0700

    Create rs-target.js

---
+++ rs-target.js
@@ -0,0 +1,20 @@
+// angular.module('myAppName').directive('rsTarget', rsTarget);
+
+function rsTarget() {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      scope.$watch(
+        function(scope) {
+          return scope.$eval(attrs.rsTarget);
+        },
+        function(targetValue) {
+          if (!targetValue)
+            element.removeAttr('taret');
+          else
+            element.attr({ target: targetValue });
+        }
+      );
+    }
+  };
+}
